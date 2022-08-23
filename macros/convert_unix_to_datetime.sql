cb7ce9273c9a47794d3e6374588c71c104ca24bd
Nnamdi <nnamdi@stakefair.io>
2022-08-23 22:13:24 +0000

    revised the convert_unix_to_datetime macros to factor in the float64 datetimes in raw_referral

--- macros/convert_unix_to_datetime.sql
+++ macros/convert_unix_to_datetime.sql
@@ -1,3 +1,8 @@
 {% macro convert_unix_to_date(unix_timestamp) %}
-    DATE(TIMESTAMP_MICROS(cast({{unix_timestamp}}/1000 as int64)))
+DATE(TIMESTAMP_MICROS(
+    CAST(
+        CAST(
+            {{unix_timestamp}} as float64)/1000 
+            as int64)))
+   
 {% endmacro %}
---
+++ models/staging/stg_referral.sql
@@ -0,0 +1,3 @@
+SELECT 
+*
+FROM {{ ref('raw_referrals') }}
