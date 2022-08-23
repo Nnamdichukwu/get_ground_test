35c8fa3bad59eac17b5aee1e75a68a644b6e3584
Nnamdi <nnamdi@stakefair.io>
2022-08-23 22:15:10 +0000

    setup the stg_referrals

--- models/staging/stg_referral.sql
+++ models/staging/stg_referral.sql
@@ -1,3 +1,9 @@
 SELECT 
-*
+is_outbound,
+id as referral_id,
+partner_id,
+consultant_id,
+{{convert_unix_to_date('created_at')}} as referral_date,
+company_id,
+status as referral_status
 FROM {{ ref('raw_referrals') }}
