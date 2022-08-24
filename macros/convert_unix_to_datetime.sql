e9a52138eeb311018694282b5893c5954d120d17
Nnamdi <nnamdi@stakefair.io>
2022-08-24 21:37:18 +0000

    Added some comments to make code more readable

--- macros/convert_unix_to_datetime.sql
+++ macros/convert_unix_to_datetime.sql
@@ -1,3 +1,5 @@
+--Converts the unix timestamp first to a float64, then to int64 before using the date function
+-- Using timestamp_micros because using seconds will lead to an overflow. 
 {% macro convert_unix_to_date(unix_timestamp) %}
 DATE(TIMESTAMP_MICROS(
     CAST(
