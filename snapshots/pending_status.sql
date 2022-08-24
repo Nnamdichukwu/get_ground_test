f58f63141f5b4193e02db2f66884c77238ef64fe
Nnamdi <nnamdi@stakefair.io>
2022-08-24 22:45:39 +0000

    Added snapshots to keep track of pending referrals

---
+++ snapshots/pending_status.sql
@@ -0,0 +1,15 @@
+{% snapshot status_snapshot %}
+
+{{
+    config(
+      target_database='getgroundtest-360217',
+      target_schema='snapshots',
+      unique_key='id',
+      strategy='timestamp',
+      updated_at='updated_at',
+    )
+}}
+
+select * from {{ source('getground', 'referrals_table') }}
+
+{% endsnapshot %}
