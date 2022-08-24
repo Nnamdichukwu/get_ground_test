72b0e5da1ddf75cd973b8ac5bc9e0fe3afa47dda
Nnamdi <nnamdi@stakefair.io>
2022-08-24 22:57:31 +0000

    Created snapshot and model to keep track of referral statuses

--- snapshots/check_statuses.sql
+++ snapshots/check_statuses.sql
@@ -1,12 +1,12 @@
-{% snapshot status_snapshot %}
+{% snapshot statuses_snapshot %}
 
 {{
     config(
       target_database='getgroundtest-360217',
       target_schema='snapshots',
       unique_key='id',
-      strategy='timestamp',
-      updated_at='updated_at',
+      strategy='check',
+      check_cols='all',
     )
 }}
 
